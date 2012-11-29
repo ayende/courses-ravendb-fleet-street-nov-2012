39a1aa450c95eb65b3a2020de3dfc1c5b2fc1042
Ayende Rahien <ayende@ayende.com>
2012-11-29 11:24:09 +0000

    Basic model & its usage

---
+++ Eastwind/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Eastwind.Models;
+
+namespace Eastwind.Controllers
+{
+	public class HomeController : RavenController
+	{
+		public ActionResult Index()
+		{
+			var users = Session.Query<User>()
+			                   .Take(3)
+			                   .ToList();
+
+			return Json(new
+				{
+					users.Count,
+					Results = users
+				});
+		}
+	}
+}
---
+++ Eastwind/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Eastwind.Models
+{
+	public class User
+	{
+		public string Gender { get; set; }
+		public string GivenName { get; set; }
+		public string Surname { get; set; }
+		public string StreetAddress { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public int ZipCode { get; set; }
+		public string EmailAddress { get; set; }
+		public string Username { get; set; }
+		public string NationalID { get; set; }
+		public string Occupation { get; set; }
+		public string Company { get; set; }
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+	}
+}
