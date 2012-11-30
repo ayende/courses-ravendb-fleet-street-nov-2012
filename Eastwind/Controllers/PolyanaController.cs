d7b3faf31c520e1818f7bc54ce1331d53da2eb84
Ayende Rahien <ayende@ayende.com>
2012-11-30 13:49:07 +0000

    Work

--- Eastwind/Controllers/PolyanaController.cs
+++ Eastwind/Controllers/PolyanaController.cs
@@ -1,48 +1,66 @@
 ﻿using System.Web.Mvc;
+using Eastwind.Indexes;
 using Eastwind.Models;
 using System.Linq;
+using Raven.Client;
 
 namespace Eastwind.Controllers
 {
 	public class PolyanaController : RavenController
 	{
-		 public ActionResult Dog()
-		 {
-			 Session.Store(new Dog
-				 {
-					 Name = "a"
-				 });
-			 return Json(null);
-		 }
+		public ActionResult Item(string cust)
+		{
+			var stats = Session.Query<DogsAndCatsByCustomer.Result, DogsAndCatsByCustomer>()
+					.Include(x => x.Customer)
+					.First(x => x.Customer == cust);
 
-		 public ActionResult Cat()
-		 {
-			 Session.Store(new Cat
-			 {
-				 Name = "a"
-			 });
-			 return Json(null);
-		 }
+			var customer = Session.Load<Customer>(stats.Customer);
 
-		 public ActionResult LoadDog(int id)
-		 {
-			 return Json(Session.Load<Dog>(id));
-		 }
+			return Json(new
+				{
+					customer.Name,
+					stats.CatCount,
+					stats.DogCount,
+				});
+		}
 
-		 public ActionResult LoadCat(int id)
-		 {
-			 return Json(Session.Load<Cat>(id));
-		 }
+		public ActionResult Dog()
+		{
+			Session.Store(new Dog
+				{
+					Name = "a"
+				});
+			return Json(null);
+		}
 
-		 public ActionResult LoadAnimal(int id)
-		 {
-			 return Json(Session.Load<Animal>(id));
-		 }
+		public ActionResult Cat()
+		{
+			Session.Store(new Cat
+			{
+				Name = "a"
+			});
+			return Json(null);
+		}
 
-		 public ActionResult QueryAnimals(int id)
-		 {
-			 return Json(Session.Query<Animal>().ToList());
-		 }
+		public ActionResult LoadDog(int id)
+		{
+			return Json(Session.Load<Dog>(id));
+		}
+
+		public ActionResult LoadCat(int id)
+		{
+			return Json(Session.Load<Cat>(id));
+		}
+
+		public ActionResult LoadAnimal(int id)
+		{
+			return Json(Session.Load<Animal>(id));
+		}
+
+		public ActionResult QueryAnimals(int id)
+		{
+			return Json(Session.Query<Animal>().ToList());
+		}
 
 	}
 }
---
+++ Eastwind/Indexes/DogsAndCatsByCustomer.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using Eastwind.Models;
+using Raven.Client.Indexes;
+
+namespace Eastwind.Indexes
+{
+	public class DogsAndCatsByCustomer : AbstractMultiMapIndexCreationTask<DogsAndCatsByCustomer.FinalResult>
+	{
+		public class Result
+		{
+			public string Customer { get; set; }
+			public int CatCount { get; set; }
+			public int DogCount { get; set; }
+		}
+
+		public class FinalResult
+		{
+			public string CustomerId { get; set; }
+			public string CustomerName { get; set; }
+			public int CatCount { get; set; }
+			public int DogCount { get; set; }
+		
+		}
+
+		public DogsAndCatsByCustomer()
+		{
+			AddMap<Dog>(dogs =>
+			            from dog in dogs
+			            from c in dog.Customers
+			            select new
+				            {
+					            CustomerId = c,
+					            CatCount = 0,
+					            DogCount = 1,
+					            CustomerName = (string) null
+				            });
+
+			AddMap<Cat>(cats =>
+			            from cat in cats
+			            from c in cat.Customers
+			            select new
+				            {
+					            CustomerId = c, 
+								CatCount = 1, 
+								DogCount = 0, 
+								CustomerName = (string) null
+				            });
+
+			AddMap<Customer>(customers =>
+			                 from customer in customers
+			                 select new
+				                 {
+									 CustomerName = customer.Name,
+									 CustomerId = customer.Id,
+									 CatCount = 0,
+									 DogCount = 0
+				                 });
+
+			Reduce = results =>
+			         from result in results
+			         group result by result.CustomerId
+			         into g
+			         select new
+				         {
+					         CustomerId = g.Key,
+					         CatCount = g.Sum(x => x.CatCount),
+					         DogCount = g.Sum(x => x.DogCount),
+							 g.FirstOrDefault(x=>x.CustomerName != null).CustomerName
+				         };
+
+
+		}
+	}
+}
